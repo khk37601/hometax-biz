cfafe01529e1ebc41ffc1d4d6c59f2592bfcb967
JS <khk37601@gmail.com>
2023-01-17 14:31:22 +0900

    Update mail.py

--- mail.py
+++ mail.py
@@ -12,11 +12,11 @@ def send_mail(
     config: Dict[str, str] = EnvironmentManager()
     with smtplib.SMTP(config.get('EMAIL_SERVER'), 587) as session:
         session.starttls()
-        session.login('help@martroo.com', config.get('EMAIL_PASSWORD'))
+        session.login('help@mxxx.com', config.get('EMAIL_PASSWORD'))
         message = MIMEMultipart()
         message['From'] = 'help@martroo.com'
         message['Subject'] = data.get('subject')
         message.attach(MIMEText(data.get('body')))
         message['To'] = ','.join(_to)
-        session.sendmail('help@martroo.com', _to, message.as_string())
+        session.sendmail('help@xxx.com', _to, message.as_string())
     del config
